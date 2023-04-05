700d37432071eb95702e7391e1706f3deccf2c3e
graci <gamaizo@iu.edu>
2023-04-04 21:21:56 -0400

    Done

--- romanToInteger.py
+++ romanToInteger.py
@@ -1,4 +1,7 @@
 class Solution(object):
     def singleNumber(self, nums: list[int])->int:
         count = Counter(nums)
+        for i in count:
+            if count[i]==1:
+                return i
 
