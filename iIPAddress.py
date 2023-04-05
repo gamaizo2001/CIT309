774730e4a550e88bd31f8a6b83be6d87b3a62061
graci <gamaizo@iu.edu>
2023-04-05 17:31:40 -0400

    done with Defanging an IP Address

--- iIPAddress.py
+++ iIPAddress.py
@@ -1,2 +1,7 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
+        """
+        Return a defanged IP address from a given IPv4 address.
+        """
+        defanged_address = address.replace('.', '[.]')
+        return defanged_address
